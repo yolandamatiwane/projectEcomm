af9c434943d7bf7479b7ba3ed1da6c18f2ae6728
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-06-10 22:46:38 -0700

    added js

---
+++ script/product.js
@@ -0,0 +1,7 @@
+let searchBtn = document.querySelector('#searching')
+let sortBtn = document.querySelector('#sorting')
+let inp = document.querySelector('#inp')
+
+searchBtn.addEventListener('click',()=>{
+    let searchItem = inp.value.toLowerCase()
+})
