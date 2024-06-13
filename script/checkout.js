3ced055c7a3dae49f11d00cd375e13d03720e07d
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-06-13 02:15:05 -0700

    made changes

--- script/checkout.js
+++ script/checkout.js
@@ -1,22 +1,45 @@
 let tbody = document.querySelector('tbody')
 let cartDis = localStorage.getItem('purchasedItems')
 let items = JSON.parse(localStorage.getItem('purchasedItems')) || []
-
-tbody.innerHTML = `<div>Hehehehehehehehehe</div>`
+// spinner
+// tbody.innerHTML = `<div>Hehehehehehehehehe</div>`
 // setTimeout(function() {
+    
 // },1000)
-
 tbody.innerHTML = ``
     items.forEach(item=>{
     tbody.innerHTML += `
-                    <tr>
-                        <td>${item.id}</td>
+                    <tr  class="table-light">
                         <td>${item.title}</td>
-                        <td>${item.price}</td>
-                        <td>${item.quantity}</td>
-                        <td> </td>
+                        <td><span id="amount">${item.price}</span></td>
+                        <td><input type="number" id="inp" value="${item.quantity}"></td>
+                        <td id="smlTot"> </td>
                     </tr>
 
                 `
                
 })
+
+// calculation
+let inp = document.querySelectorAll('#inp')
+
+inp.forEach((input,index)=>{
+    input.addEventListener('input',()=>{
+        subtotal(index)
+        calculateTotal()
+    })
+})
+// Function to update subtotal for a given item index
+function updateSubtotal(index) {
+    let quantity = parseInt(inp1[index].value);
+    let price = parseFloat(items[index].price);
+    let subtotal = quantity * price;
+    smTot[index].textContent = subtotal.toFixed(2);
+}
+
+// Function to calculate total
+function calculateTotal() {
+    let subtotalArray = Array.from(smTot).map(td => parseFloat(td.textContent));
+    let totalPrice = subtotalArray.reduce((acc, val) => acc + val, 0);
+    total.textContent = totalPrice.toFixed(2);
+}
--- script/product.js
+++ script/product.js
@@ -13,30 +13,40 @@ function createBooks(id,title,author,category,image,price,quantity,description){
 
 let book_1 = new createBooks(1,'Three Little Pigs','Joseph Jacobs','Children Literature','https://yolandamatiwane.github.io/ecomImages/books/three-little-pigs.jpg',120.99,1,'"The Three Little Pigs" is a classic fairy tale about three sibling pigs who each build a house out of different materials—straw, sticks, and bricks—only to face the threat of a cunning wolf. It\'s a timeless story of resourcefulness, perseverance, and the importance of solid foundations.')
 let book_2 = new createBooks(2,'Sulwe','Lupita Nyongo','Children Literature','https://yolandamatiwane.github.io/ecomImages/books/Sulwe-by-Lupita-Nyong%E2%80%99o.jpg',150.99,1,'"Sulwe" by Lupita Nyong\'o is a captivating children\'s book that follows the journey of a young girl named Sulwe who struggles with her self-esteem because of her darker skin tone. Through magical encounters and self-discovery, Sulwe learns to embrace her unique beauty and value. This heartfelt story celebrates diversity, self-acceptance, and the beauty found within every individual.') 
-let book_3 = new createBooks(3,'It ends with Us','Colleen Hoover','Young Adult Romance','https://yolandamatiwane.github.io/ecomImages/books/27362503.jpg',345.99,1,'"It Ends with Us" is a powerful novel by Colleen Hoover that delves into the complexities of love, abuse, and self-discovery. The story follows Lily Bloom as she navigates a tumultuous relationship with a charismatic yet troubled man named Ryle Kincaid. As secrets unravel and past traumas resurface, Lily must confront difficult choices about her own happiness and well-being. This emotionally charged narrative explores themes of resilience, forgiveness, and the strength it takes to break cycles of abuse.')
+let book_3 = new createBooks(3,'Queens of Geek','Jen Wilde','Young Adult','https://yolandamatiwane.github.io/ecomImages/books/28245707.jpg',300,1,'The story follows three friends as they attend SupaCon, a fictional version of a pop culture convention. The narrative explores themes of friendship, romance, and identity, with a focus on LGBTQ+ representation and fandom culture. It celebrates diversity and empowerment, making it a beloved read among fans of contemporary YA literature.')
+let book_4 = new createBooks(4,'Red, White & Royal Blue','Casey McQuiston','Young Adult','https://yolandamatiwane.github.io/ecomImages/books/41150487.jpg',350.99,1,'It falls within the romance and LGBTQ+ fiction genres. The story revolves around the unlikely romance between Alex Claremont-Diaz, the First Son of the United States, and Prince Henry of Wales. Set in an alternate reality where a woman is President of the United States, the novel explores themes of love, politics, and identity. It\'s a charming and witty tale that captivates readers with its humor, heartwarming moments, and compelling characters.')
+let book_5 = new createBooks(5,'Simone Breaks all the Rules','Debbie Rigaud','Young Adult','https://yolandamatiwane.github.io/ecomImages/books/53200402.jpg',250,1,'The story follows sixteen-year-old Simone Thibodeaux, who decides to break free from her strict Haitian immigrant parents\' rules by pursuing a secret relationship with a charming and rebellious classmate named Miles. As Simone navigates the complexities of family expectations, cultural identity, and teenage romance, she learns valuable lessons about love, independence, and staying true to herself. The novel explores themes of self-discovery, cultural heritage, and the courage to challenge societal norms.')
+let book_6 = new createBooks(6,'Clifford The Big Red Dog','Norman Bridwell','Children\'s Literature','https://yolandamatiwane.github.io/ecomImages/books/858719.jpg',100,1,'he series follows the adventures of a giant red dog named Clifford and his owner, Emily Elizabeth. Clifford\'s enormous size leads to humorous and heartwarming situations as he interacts with the world around him, teaching young readers lessons about friendship, kindness, and acceptance. The books are beloved for their colorful illustrations and simple yet engaging storytelling, making them a timeless favorite among children and parents alike.')
 
-let books = [book_1,book_2,book_3]
+let books = [book_1,book_2,book_3,book_4,book_5,book_6]
 
 localStorage.setItem('books',JSON.stringify(books))
 
 //shows default
+
 function display(booksArray){
     article.innerHTML = ``
-    booksArray.forEach(book=>{
-    article.innerHTML+=`
-                    <div class="card" id='cards'>
-                    <img src='${book.image}'>
-                    <div class="card-body">
-                    ${book.price}
-                    <br>
-                    <button id="viewMore">View More</button><button id="purchase" value="${book.id}">ADD to Cart</button>
-                    <div>
+    if(booksArray.length === 0){
+        article.innerHTML = `<p> Sorry! Item was not found. </p>`
+    }else{
+        booksArray.forEach(book=>{
+        article.innerHTML+=`
+                        <div class="card" id='cards'>
+                        <img class="card-img-top" src='${book.image}'>
+                        <div class="card-body">
+                        <h6 class="card-title">${book.title}</h6>
+                        R${book.price}
+                        <br>
+                        <button type="button" class="btn btn-outline-pink" id="viewMore">View More</button><button type="button" class="btn btn-outline-pink" id="purchase" value="${book.id}">ADD to Cart</button>
+                        <div>
+    
+                        </div>
+    
+    
+                     `
+    })}
 
-                    </div>
-
-
-                 `
-})}
+    }
 display(books)
 let searchBtn = document.querySelector('#searching')
 let resetBtn = document.querySelector('#searchingReset')
@@ -60,12 +70,11 @@ searchBtn.addEventListener('click',()=>{
 
 //part1 of checkout
 let purchasedBtn = document.querySelectorAll('#purchase')
-let purchasedItems = JSON.parse(localStorage.getItem('purchasedItems')) || []
+let purchasedItems =[]
 function addToCheckout(id){
     let [item] = books.filter(object=> object.id === +id)
     purchasedItems.push(item)
     console.log(purchasedItems)
-
 }
 purchasedBtn.forEach(button=>{
     button.addEventListener('click',(event)=>{
@@ -92,3 +101,6 @@ selected.addEventListener('change',()=>{
     display(sort)
 
 })
+
+
+// still need to add a spinner for books=[] and sort by price
