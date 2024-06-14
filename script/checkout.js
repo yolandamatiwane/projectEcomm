44509378c54514543b473dbb3384729a9f1c5e01
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-06-14 02:27:17 -0700

    added checkout data

--- script/checkout.js
+++ script/checkout.js
@@ -1,29 +1,50 @@
 let tbody = document.querySelector('tbody')
 let cartDis = localStorage.getItem('purchasedItems')
 let items = JSON.parse(localStorage.getItem('purchasedItems')) || []
+let tyBtn = document.querySelector('#submitPurchase')
 let grandtotal = 0
 // spinner
 // tbody.innerHTML = `<div>Hehehehehehehehehe</div>`
 // setTimeout(function() {
     
 // },1000)
+let total = document.querySelector('#ans')
 tbody.innerHTML = ``
-items.forEach((item,index)=>{
+items.forEach(item=>{
     let subtotal = (item.price * item.quantity).toFixed(2)
-    // console.log(grandtotal)
+    grandtotal += parseFloat(subtotal) 
+    total.innerText = grandtotal
+    
+    console.log(grandtotal)
     tbody.innerHTML += `
                         <tr  class="table-light">
                             <td>${item.title}</td>
                             <td><span id="amount">${item.price}</span></td>
                             <td><input type="number" id="inp" value="${item.quantity}"></td>
                             <td><span id="smlTot">${subtotal}</span></td>
-                            <td class="delete" data-id="${item.id}"> delete</td>
+                            <td class="delete" data-id="${item.id}"> x</td>
                         </tr>
 
                         `
                
 })
+// updating totals
 
+let inputs = document.querySelectorAll('#inp')
+inputs.forEach(input=>{
+    input.addEventListener('change',()=>{
+        let grandtotal = 0
+        inputs.forEach((input,i)=>{
+            let quantity = parseInt(input.value)
+            items[i].quantity = quantity
+            let subTotal = (items[i].price*quantity).toFixed(2)
+            document.querySelectorAll('#smlTot')[i].innerText = subTotal
+            grandtotal +=parseFloat(subTotal)
+        })
+        total.innerText = grandtotal.toFixed(2)
+    })
+})
+//still need to update local storage
 // remove from cart
 let remove = document.querySelectorAll('.delete')
 
@@ -36,6 +57,11 @@ function removed(id) {
 remove.forEach(btn => {
     btn.addEventListener('click', (event) => {
         removed(event.target.dataset.id)
-        event.target.closest('tr').remove();
+        event.target.closest('tr').remove()
     })
+})
+
+// Thank You Message
+tyBtn.addEventListener('click',()=>{
+    alert('Thank You For Choosing Us <3')
 })
